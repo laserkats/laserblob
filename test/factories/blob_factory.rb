edd5de91d8f4e11b40b1214b795c2be8ec2029e4
Ben Ehmke <benehmke@gmail.com>
2026-02-04 14:34:53 -0600

    Spreadsheet Type (#2)
    * add spreadsheet type
    
    * add tests
    
    * update process! test

--- test/factories/blob_factory.rb
+++ test/factories/blob_factory.rb
@@ -33,6 +33,24 @@ FactoryBot.define do
     end
   end
 
+  factory :spreadsheet, class: 'LaserBlob::Blob::Spreadsheet' do
+    file do
+      f = Tempfile.new(['spreadsheet', '.csv'], binmode: true)
+      f.write("col1,col2,col3\nval1,val2,val3\n")
+      f.flush
+      Rack::Test::UploadedFile.new(f.path, 'text/csv', true)
+    end
+  end
+
+  factory :xlsx, class: 'LaserBlob::Blob::Spreadsheet' do
+    file do
+      f = Tempfile.new(['spreadsheet', '.xlsx'], binmode: true)
+      f.write(SecureRandom.random_bytes(100))
+      f.flush
+      Rack::Test::UploadedFile.new(f.path, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', true)
+    end
+  end
+
   factory :attachment, class: 'LaserBlob::Attachment' do
     association :blob
     filename { 'test.bin' }
---
+++ test/models/laser_blob/blob/spreadsheet_test.rb
@@ -0,0 +1,96 @@
+require 'test_helper'
+
+class LaserBlob::Blob::SpreadsheetTest < ActiveSupport::TestCase
+
+  test 'spreadsheet type is assigned for spreadsheet content types' do
+    LaserBlob::Blob::Spreadsheet::CONTENT_TYPES.each do |content_type|
+      blob = LaserBlob::Blob.new(data: 'fake spreadsheet', content_type: content_type)
+      assert_equal LaserBlob::Blob::Spreadsheet, blob.class, "Expected Spreadsheet for #{content_type}"
+    end
+  end
+
+  test 'spreadsheet validates content_type' do
+    blob = LaserBlob::Blob::Spreadsheet.new(data: 'test', content_type: 'text/plain')
+    blob.size = 4
+    blob.sha1 = Digest::SHA1.digest('test')
+    assert_not blob.valid?
+    assert blob.errors[:content_type].present?
+  end
+
+  test 'sheet_count accessor' do
+    blob = LaserBlob::Blob::Spreadsheet.new
+    blob.metadata = { 'sheets' => [
+      { 'name' => 'Sheet1', 'rows' => 10, 'columns' => 5 },
+      { 'name' => 'Sheet2', 'rows' => 20, 'columns' => 3 }
+    ] }
+
+    assert_equal 2, blob.sheet_count
+  end
+
+  test 'row_count accessor sums rows across sheets' do
+    blob = LaserBlob::Blob::Spreadsheet.new
+    blob.metadata = { 'sheets' => [
+      { 'name' => 'Sheet1', 'rows' => 10, 'columns' => 5 },
+      { 'name' => 'Sheet2', 'rows' => 20, 'columns' => 3 }
+    ] }
+
+    assert_equal 30, blob.row_count
+  end
+
+  test 'column_count accessor returns max columns' do
+    blob = LaserBlob::Blob::Spreadsheet.new
+    blob.metadata = { 'sheets' => [
+      { 'name' => 'Sheet1', 'rows' => 10, 'columns' => 5 },
+      { 'name' => 'Sheet2', 'rows' => 20, 'columns' => 3 }
+    ] }
+
+    assert_equal 5, blob.column_count
+  end
+
+  test 'sheets accessor returns sheets array' do
+    sheets = [
+      { 'name' => 'Sheet1', 'rows' => 10, 'columns' => 5 },
+      { 'name' => 'Sheet2', 'rows' => 20, 'columns' => 3 }
+    ]
+    blob = LaserBlob::Blob::Spreadsheet.new
+    blob.metadata = { 'sheets' => sheets }
+
+    assert_equal sheets, blob.sheets
+  end
+
+  test 'accessors return defaults when metadata is empty' do
+    blob = LaserBlob::Blob::Spreadsheet.new
+    blob.metadata = {}
+
+    assert_equal 0, blob.sheet_count
+    assert_equal 0, blob.row_count
+    assert_equal 0, blob.column_count
+    assert_equal [], blob.sheets
+  end
+
+  test 'extension_for_content_type returns correct extensions' do
+    assert_equal :csv, LaserBlob::Blob::Spreadsheet.extension_for_content_type('text/csv')
+    assert_equal :csv, LaserBlob::Blob::Spreadsheet.extension_for_content_type('application/csv')
+    assert_equal :xls, LaserBlob::Blob::Spreadsheet.extension_for_content_type('application/vnd.ms-excel')
+    assert_equal :xlsx, LaserBlob::Blob::Spreadsheet.extension_for_content_type('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+  end
+
+  test 'process! extracts spreadsheet metadata' do
+    begin
+      require 'roo'
+    rescue LoadError
+      skip "Requires roo gem"
+    end
+
+    spreadsheet = LaserBlob::Blob.create!(
+      file: Rack::Test::UploadedFile.new(FIXTURES.join('test.csv'), 'text/csv', true)
+    )
+    spreadsheet.process!
+
+    assert_equal 1, spreadsheet.sheet_count
+    assert_equal 4, spreadsheet.row_count
+    assert_equal 3, spreadsheet.column_count
+    assert_equal [{ 'name' => 'default', 'rows' => 4, 'columns' => 3 }], spreadsheet.sheets
+  end
+
+end
