0e21b5aea998df75e7e83422f9bc8e2393d2072a
Ben Ehmke <benehmke@gmail.com>
2026-02-04 12:02:22 -0600

    add tests

--- test/dummy/app/models/document.rb
+++ test/dummy/app/models/document.rb
@@ -4,3 +4,12 @@ class Document < ApplicationRecord
   has_one_blob :file
   has_many_blobs :attachments
 end
+
+class ImageDocument < ApplicationRecord
+  self.table_name = 'documents'
+
+  include LaserBlob::ModelExtensions
+
+  has_one_blob :cover, only: [:image]
+  has_many_blobs :photos, only: [LaserBlob::Blob::Image]
+end
